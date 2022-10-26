1a24536e60fc1c288d56b4d15436f01ae4407760
codekim97 <kimtaeu4616@gmail.com>
2022-10-26 16:11:55 +0900

    첫 commit

---
+++ app/src/main/java/com/example/kotlinproject/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlinproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
