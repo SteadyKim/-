960bca29d8cfd431863a58e85cdf6f7273505191
codekim97 <kimtaeu4616@gmail.com>
2022-11-15 21:25:25 +0900

    카카오톡 공유하기

--- app/src/main/java/com/example/kotlinproject/HistoryActivity.kt
+++ app/src/main/java/com/example/kotlinproject/HistoryActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinproject
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +11,12 @@ import com.example.kotlinproject.db.entity.Foods
 import com.example.kotlinproject.enum.FoodNames
 import com.example.kotlinproject.recyclerview.RecyclerViewAdapter
 import com.google.firebase.database.*
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.KakaoCustomTabsClient
+import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.share.ShareClient
+import com.kakao.sdk.share.WebSharerClient
+import com.kakao.sdk.template.model.*
 import java.time.LocalDate
 import java.util.*
 
@@ -28,6 +35,17 @@ class HistoryActivity : AppCompatActivity() {
 
         val binding = ActivityHistoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnKakao.setOnClickListener {
+            //Kakao 공유
+            val keyHash = Utility.getKeyHash(this)
+//        println("keyHash = ${keyHash}")
+
+            KakaoSdk.init(this, "a3371d68064ad62c0ef12f967df3741c")
+            val defaultFeed = makeKakaoTemplate()
+            //카카오톡 설치 여부 확인
+            startKakaoLink(defaultFeed)
+        }
 
         //DB 초기화
         db = AppDatabase.getInstance(this)
@@ -86,6 +104,98 @@ class HistoryActivity : AppCompatActivity() {
 
         //버튼 클릭시 ChartActivity로 이통
         initChartBtn(binding)
+    }
+
+    private fun makeKakaoTemplate(): FeedTemplate {
+        val defaultFeed = FeedTemplate(
+            content = Content(
+                title = "오늘의 디저트",
+                description = "#케익 #딸기 #삼평동 #카페 #분위기 #소개팅",
+                imageUrl = "http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+                link = Link(
+                    webUrl = "https://developers.kakao.com",
+                    mobileWebUrl = "https://developers.kakao.com"
+                )
+            ),
+            itemContent = ItemContent(
+                profileText = "Kakao",
+                profileImageUrl = "http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+                titleImageUrl = "http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+                titleImageText = "Cheese cake",
+                titleImageCategory = "Cake",
+                items = listOf(
+                    ItemInfo(item = "cake1", itemOp = "1000원"),
+                    ItemInfo(item = "cake2", itemOp = "2000원"),
+                    ItemInfo(item = "cake3", itemOp = "3000원"),
+                    ItemInfo(item = "cake4", itemOp = "4000원"),
+                    ItemInfo(item = "cake5", itemOp = "5000원")
+                ),
+                sum = "Total",
+                sumOp = "15000원"
+            ),
+            social = Social(
+                likeCount = 286,
+                commentCount = 45,
+                sharedCount = 845
+            ),
+            buttons = listOf(
+                Button(
+                    "웹으로 보기",
+                    Link(
+                        webUrl = "https://developers.kakao.com",
+                        mobileWebUrl = "https://developers.kakao.com"
+                    )
+                ),
+                Button(
+                    "앱으로 보기",
+                    Link(
+                        androidExecutionParams = mapOf("key1" to "value1", "key2" to "value2"),
+                        iosExecutionParams = mapOf("key1" to "value1", "key2" to "value2")
+                    )
+                )
+            )
+        )
+        return defaultFeed
+    }
+
+    private fun startKakaoLink(defaultFeed: FeedTemplate) {
+        if (ShareClient.instance.isKakaoTalkSharingAvailable(this)) {
+            // 카카오톡으로 카카오톡 공유 가능
+            ShareClient.instance.shareDefault(this, defaultFeed) { sharingResult, error ->
+                if (error != null) {
+                    Log.e(TAG, "카카오톡 공유 실패", error)
+                } else if (sharingResult != null) {
+                    Log.d(TAG, "카카오톡 공유 성공 ${sharingResult.intent}")
+                    startActivity(sharingResult.intent)
+
+                    // 카카오톡 공유에 성공했지만 아래 경고 메시지가 존재할 경우 일부 컨텐츠가 정상 동작하지 않을 수 있습니다.
+                    Log.w(TAG, "Warning Msg: ${sharingResult.warningMsg}")
+                    Log.w(TAG, "Argument Msg: ${sharingResult.argumentMsg}")
+                }
+            }
+        } else {
+            // 카카오톡 미설치: 웹 공유 사용 권장
+            // 웹 공유 예시 코드
+            val sharerUrl = WebSharerClient.instance.makeDefaultUrl(defaultFeed)
+
+            // CustomTabs으로 웹 브라우저 열기
+
+            // 1. CustomTabsServiceConnection 지원 브라우저 열기
+            // ex) Chrome, 삼성 인터넷, FireFox, 웨일 등
+            try {
+                KakaoCustomTabsClient.openWithDefault(this, sharerUrl)
+            } catch (e: UnsupportedOperationException) {
+                // CustomTabsServiceConnection 지원 브라우저가 없을 때 예외처리
+            }
+
+            // 2. CustomTabsServiceConnection 미지원 브라우저 열기
+            // ex) 다음, 네이버 등
+            try {
+                KakaoCustomTabsClient.open(this, sharerUrl)
+            } catch (e: ActivityNotFoundException) {
+                // 디바이스에 설치된 인터넷 브라우저가 없을 때 예외처리
+            }
+        }
     }
 
     private fun initPlusBtn_나중에지울예정(binding: ActivityHistoryBinding, adapter: RecyclerViewAdapter) {
